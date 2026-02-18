d932a088d75f3904bddb289177736ffaccd04c43
Mati Ur Rehman <matiurrehmanmarwatkhan@gmail.com>
2026-02-18 22:03:11 +0500

    Changes

--- script.js
+++ script.js
@@ -4,37 +4,3 @@ const navLinks = document.getElementById("navLinks");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
-
-const text = [
-  "Frontend Developer",
-  "Backend Developer",
-  "Fullstack Developer",
-  "Mobile Developer",
-];
-
-let count = 0;
-let index = 0;
-let del = false;
-
-const textElement = document.getElementById("text");
-
-setInterval(() => {
-  if (!del) {
-    // Typing
-    index++;
-    textElement.textContent = text[count].slice(0, index);
-
-    if (index === text[count].length) {
-      del = true; // start deleting
-    }
-  } else {
-    // Deleting
-    index--;
-    textElement.textContent = text[count].slice(0, index);
-
-    if (index === 0) {
-      del = false;
-      count = (count + 1) % text.length; // next word
-    }
-  }
-}, 120);
